b25d467faca9d72e1612be79d72592d208b26903
Ravi kumar <1002kumarravi@gmail.com>
2023-01-19 16:30:55 +0530

    keyboard folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import reactLogo from './assets/react.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import KeyBoard from "@components/keyBoard";
 
 function App() {
-
   return (
     <div className="App">
-      
+      asdasd
+      <KeyBoard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
