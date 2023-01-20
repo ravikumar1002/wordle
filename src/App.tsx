c8d5559fdb522e606b39e66e4336445277d10fdb
Ravi kumar <1002kumarravi@gmail.com>
2023-01-20 19:14:23 +0530

    add word generate logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import "./App.css";
 import KeyBoard from "@components/keyBoard";
 import Board from "@components/board";
 import {Typography} from "@mui/material";
+import { generateWordSet } from "utils/words";
 
 function App() {
+
+  generateWordSet()
   return (
     <div className="App">
       <Typography variant="h2" component="h2" textAlign={"center"}>
