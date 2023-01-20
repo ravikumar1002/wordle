1aac497bccd15407fae5b80f7cb3daee77e7e989
Ravi kumar <1002kumarravi@gmail.com>
2023-01-20 10:19:37 +0530

    board layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import KeyBoard from "@components/keyBoard";
+import Board from "@components/board";
+import {Typography} from "@mui/material";
 
 function App() {
   return (
     <div className="App">
+      <Typography variant="h2" component="h2" textAlign={"center"}>
+        Wordle
+      </Typography>
+      <Board />
       <KeyBoard />
     </div>
   );
